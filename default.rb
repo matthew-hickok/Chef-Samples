9ef10c5bdda2f632a8db4b2ddebe38156c76248e
Matt Hickok <mhickok@netrixllc.com>
2017-09-01 00:00:33 -0500

    added sample filr for blog post

---
+++ default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook:: utility
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+['RSAT-ADDS-Tools','Hyper-V-Tools','GPMC','Web-Mgmt-Tools','Web-Mgmt-Console','Web-Ftp-Server','Print-Server'].each do |feature|
+    dsc_resource 'Ensure Utility Server Components Installed' do
+        resource :WindowsFeature
+        property :Name, feature
+        property :Ensure, 'Present'
+    end
+end
+
+powershell_script 'Install xWebAdmin' do
+    code 'Install-Module -Name xWebAdministration -SkipPublisherCheck -Force'
+    only_if '!(Get-Module xWebAdministration -ListAvailable)'
+  end
+
+dsc_resource 'Ensure FTP Service is automatic and running' do
+    resource :Service
+    property :Name, 'FTPSVC'
+    property :StartupType, 'Automatic'
+    property :State, 'Running'
+end
+
+dsc_resource 'Ensure Print Service is automatic and running' do
+  resource :Service
+  property :Name, 'Spooler'
+  property :StartupType, 'Automatic'
+  property :State, 'Running'
+end
